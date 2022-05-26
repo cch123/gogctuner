870df6646e95124ba880409ec897fc0b0f876fb4
Ashton Kinslow <github@ashtonkinslow.com>
2022-05-26 17:48:02 -0500

    add tests

--- tuner.go
+++ tuner.go
@@ -1,11 +1,40 @@
 package gogctuner
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 	"runtime/debug"
 	"strconv"
+
+	"log"
 )
+
+type ILogger interface {
+	Error(args ...interface{})
+	Debug(args ...interface{})
+}
+type OptFunc func() error
+
+func SetLogger(l ILogger) OptFunc {
+	return func() error {
+		if l != nil {
+			logger = l
+		}
+		return nil
+	}
+}
+
+type StdLoggerAdapter struct {
+}
+
+func (l *StdLoggerAdapter) Error(args ...interface{}) {
+	log.Print(args...)
+}
+
+func (l *StdLoggerAdapter) Debug(args ...interface{}) {
+	log.Print(args...)
+}
 
 type finalizer struct {
 	ref *finalizerRef
@@ -21,16 +50,19 @@ var previousGOGC = 100
 // don't trigger err log on every failure
 var failCounter = -1
 
+var logger ILogger
+
 func getCurrentPercentAndChangeGOGC() {
 	memPercent, err := getUsage()
 
 	if err != nil {
 		failCounter++
 		if failCounter%10 == 0 {
-			println("failed to adjust GC", err.Error())
+			logger.Error(fmt.Sprintf("failed to adjust GC err :%v", err.Error()))
 		}
 		return
 	}
+
 	// hard_target = live_dataset + live_dataset * (GOGC / 100).
 	// 	hard_target =  memoryLimitInPercent
 	// 	live_dataset = memPercent
@@ -40,6 +72,9 @@ func getCurrentPercentAndChangeGOGC() {
 	// if newgogc < 0, we have to use the previous gogc to determine the next
 	if newgogc < 0 {
 		newgogc = float64(previousGOGC) * memoryLimitInPercent / memPercent
+		logger.Debug(fmt.Sprintf("adjusting GOGC using previous - from %v to %v", previousGOGC, newgogc))
+	} else {
+		logger.Debug(fmt.Sprintf("adjusting GOGC - from %v to %v", previousGOGC, newgogc))
 	}
 
 	previousGOGC = debug.SetGCPercent(int(newgogc))
@@ -55,7 +90,11 @@ func finalizerHandler(f *finalizerRef) {
 //   set percent to control the gc trigger, 0-100, 100 or upper means no limit
 //
 //   modify default GOGC value in the case there's an env variable set.
-func NewTuner(useCgroup bool, percent float64) *finalizer {
+func NewTuner(useCgroup bool, percent float64, options ...OptFunc) *finalizer {
+	logger = &StdLoggerAdapter{}
+	for _, opt := range options {
+		opt()
+	}
 
 	if envGOGC := os.Getenv("GOGC"); envGOGC != "" {
 		n, err := strconv.Atoi(envGOGC)
@@ -70,6 +109,8 @@ func NewTuner(useCgroup bool, percent float64) *finalizer {
 	}
 
 	memoryLimitInPercent = percent
+
+	logger.Debug(fmt.Sprintf("GC Tuner initialized. GOGC: %v Target percentage: %v", previousGOGC, percent))
 
 	f := &finalizer{}
 
---
+++ tuner_test.go
@@ -0,0 +1,61 @@
+package gogctuner
+
+import (
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func GenerateMap(entries int) map[int]string {
+	m := map[int]string{}
+	for i := 0; i <= entries; i++ {
+		m[i] = RandStringRunes(10000)
+	}
+	return m
+}
+
+func TestInitDoesNotPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Error("The code paniced", r)
+		}
+	}()
+
+	go NewTuner(false, 70)
+
+	maps := []map[int]string{}
+	maps = append(maps, GenerateMap(2*1000))
+	runtime.GC()
+	maps = append(maps, GenerateMap(4*1000))
+	runtime.GC()
+	maps = append(maps, GenerateMap(8*1000))
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestInitDoesNotPanic2(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Error("The code paniced", r)
+		}
+	}()
+
+	go NewTuner(true, 70, SetLogger(&StdLoggerAdapter{}))
+	GenerateMap(5 * 1000)
+	time.Sleep(5 * time.Second)
+}
