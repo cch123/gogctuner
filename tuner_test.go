026c60d9c0ebe65078d6236526ab1880e2ac6c5a
Ashton Kinslow <github@ashtonkinslow.com>
2022-05-31 12:49:24 -0500

    extraxt goGC func and add basic tests

--- tuner_test.go
+++ tuner_test.go
@@ -59,3 +59,40 @@ func TestInitDoesNotPanic2(t *testing.T) {
 	GenerateMap(5 * 1000)
 	time.Sleep(5 * time.Second)
 }
+
+// func getGOGC(previousGOGC int , memoryLimitInPercent, memPercent float64) int {
+type GetGOGCTestCase struct {
+	PreviousGOGC         int
+	MemoryLimitInPercent float64
+	MemPercent           float64
+	ExpectedGOGC         int
+}
+
+func TestGetGOGCBasics(t *testing.T) {
+	given := []GetGOGCTestCase{
+		{
+			PreviousGOGC:         100,
+			MemoryLimitInPercent: 80,
+			MemPercent:           100,
+			ExpectedGOGC:         80,
+		},
+		{
+			PreviousGOGC:         10,
+			MemoryLimitInPercent: 80,
+			MemPercent:           10,
+			ExpectedGOGC:         700,
+		},
+		{
+			PreviousGOGC:         100,
+			MemoryLimitInPercent: 80,
+			MemPercent:           30,
+			ExpectedGOGC:         166,
+		},
+	}
+	for i, _ := range given {
+		result := getGOGC(given[i].PreviousGOGC, given[i].MemoryLimitInPercent, given[i].MemPercent)
+		if result != given[i].ExpectedGOGC {
+			t.Errorf("Failed Test Case #%v - Expected: %v Found: %v", i+1, given[i].ExpectedGOGC, result)
+		}
+	}
+}
